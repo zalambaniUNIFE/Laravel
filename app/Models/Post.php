292185d1b7953ccc9e7e02928741856ca7f35072
zalambaniUNIFE <alice.zalambani@edu.unife.it>
2024-04-18 11:20:16 +0200

    Progetto Laravel

---
+++ app/Models/Post.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
+
+class Post
+{
+    public $title;
+    public $excerpt;
+    public $date;
+    public $body;
+    public $slug;
+
+    public function __construct($title, $excerpt, $date, $body, $slug){
+        $this->title = $title;
+        $this->excerpt = $excerpt;
+        $this->date = $date;
+        $this->body = $body;
+        $this->slug = $slug;
+    }
+    public static function all(){
+        return cache() -> rememberForever('posts.all', function(){
+            return collect(File::files(resource_path("posts")))
+            ->map(function ($file) {
+                $document = YamlFrontMatter::parseFile($file);
+                return new Post(
+                    $document->title,
+                    $document->excerpt,
+                    $document->date,
+                    $document->body(),
+                    $document->slug
+                );
+            })->sortBy('date');
+        });
+    }
+    public static function find($slug)
+    {
+        return static::all()->where("slug", $slug)->first();
+    }
+
+    public static function findOrFail($slug)
+    {
+        $post = static::find($slug);
+        
+        if (! $post) {
+            throw new ModelNotFoundException();
+        }
+        
+        return $post;
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Models\Post;
+
+Route::get('/', function () {
+    return view('posts', [
+        'posts' => Post::all()
+    ]);
+});
+
+Route::get('posts/{post}', function ($slug) {
+    return view('post', [
+        'post'=> Post::findOrFail($slug)
+    ]);
+});
